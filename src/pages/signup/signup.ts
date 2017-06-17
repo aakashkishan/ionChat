18fdb509b48603b9712287c0337f5eec64080359
aakash kishan <aakash kishan>
2017-06-18 03:00:55 +0530

    Added SignUp Validation

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 
 // Import the Users Interface and the User Provider
 import { Users } from '../../models/interfaces/users';
@@ -20,7 +20,8 @@ export class SignupPage {
 
   newUser = {} as Users;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider: UserProvider, 
+              public loadingCtrl: LoadingController, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -29,6 +30,12 @@ export class SignupPage {
 
   // SignUp Method using the Credentials from the SignUp Page
   signup() {
+
+    // We will also use the ToastController to deliver messages to the User during the Password Validation
+    let toaster = this.toastCtrl.create({
+      position: 'bottom',
+      duration: 2000
+    }) 
 
     // We will declare and initialize a loader for Cool Animations while storing the User Credentials takes place
     let loader = this.loadingCtrl.create({
@@ -42,14 +49,35 @@ export class SignupPage {
     // We call the signUpee Method in the User Provider and the promise response can be Boolean or an JSON format Error
     this.userProvider.signUpee(this.newUser).then((response: any) => {
 
-      // Since we got the response, there is no reason for the loader. So stop it
-      loader.dismiss();
+      if(this.newUser.email == '' || this.newUser.pwd == '' || this.newUser.uname == '') {
 
-      // Check if the response is success or not
-      if(!response.code) {
-        this.navCtrl.push('PhotoPage');
-      } else {
-        alert(response);
+        // If the Condition is true, then not all the inputs were fille. So send a toast asking the user to fill all inputs
+        toaster.setMessage("All the fields are required to be filled!!");
+        toaster.present();
+
+      }
+
+      else if (this.newUser.pwd.length < 8) {
+
+        // Since the password filled by the User id less than 8 characters, the password is not Strong
+        toaster.setMessage("Password is weak!!, Password Must be more than 8 characters long.");
+        toaster.present();
+
+      }
+
+      else {
+        //No toasting here since there are no Validation problems here
+
+        // Since we got the response, there is no reason for the loader. So stop it
+        loader.dismiss();
+
+        // Check if the response is success or not
+        if(!response.code) {
+          this.navCtrl.push('PhotoPage');
+        } else {
+          alert(response);
+        }
+
       }
     })
 
